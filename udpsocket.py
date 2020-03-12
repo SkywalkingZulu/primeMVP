bda8c77f10d9126b2cfb7adf8498d8644d1edb2b
Andile Mchunu <47420082+SkywalkingZulu@users.noreply.github.com>
2020-03-12 16:16:04 +0200

    Add files via upload
    Pre chain Logic

---
+++ udpsocket.py
@@ -0,0 +1,12 @@
+import socket
+import struct
+import sys
+
+multicast_group = '224.3.29.71'
+server_address = ('', 10000)
+
+# Create the socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bind to the server address
+sock.bind(server_address)
